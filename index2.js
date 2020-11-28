ec93278dde358e0ecd56e9a9851c6c777398d333
SCOTTY0101 <alan777555@hotmail.com>
2020-11-28 21:52:34 +1000

    testing css

--- index2.js
+++ index2.js
@@ -2,24 +2,26 @@
 const http = require('http');
 const fs = require('fs');
 const port = 3000;
+//const express = require('express');
+//index2.use(express.static(__dirname + 'public'));
 
 const server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type' : 'test-html'})
     fs.readFile('index.html', function(error, data) {
         if (error) {
-            res.writeHead(404)
-            res.write('Error : File Not Found')
+            res.writeHead(404);
+            res.write('Error : File Not Found');
         } else {
-            res.write(data)
+            res.write(data);
         }
-        res.end()
-    })
-})
+        res.end();
+    });
+});
 
 server.listen(port, function(error) {
     if (error) {
-        console.log('Something want wrong', error)
+        console.log('Something want wrong', error);
     } else {
-        console.log('Server is listening on port' + port)
-    }
-})
+        console.log('Server is listening on port' + port);
+    };;
+});
